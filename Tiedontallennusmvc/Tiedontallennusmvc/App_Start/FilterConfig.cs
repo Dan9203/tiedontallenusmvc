0066551b161277dc4179a5d1f294bf70d1236d01
Dan Äikäs <dan.aikas@stud.pointcollege.fi>
2018-01-18 12:26:40 +0200

    luotu uusi prjoketi

---
+++ Tiedontallennusmvc/Tiedontallennusmvc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tiedontallennusmvc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Tiedontallennusmvc/Tiedontallennusmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tiedontallennusmvc.MvcApplication" Language="C#" %>
